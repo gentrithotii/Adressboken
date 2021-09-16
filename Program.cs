e3248788f681a9a8ac53b4d63fb8e85bdf4d0754
Gentrit <gentrithoti@hotmail.com>
2021-09-16 11:15:15 +0200

    Ändrade texten lite bara

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ while (true)
     Console.ForegroundColor = ConsoleColor.Yellow;
     Console.WriteLine($"~: ADRESSBOKEN ({numberOfPeopleInList} personer, {contacts.Length} tecken) :~\n");
     Console.ResetColor();
-    Console.WriteLine("[V]isa personer");
+    Console.WriteLine("[V]isa namnen på personerna ");
     Console.WriteLine("[L]ägg till person");
     Console.WriteLine("\n[A]vsluta [R]ensa adressboken");
 
